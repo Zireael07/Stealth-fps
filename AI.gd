3fd28473abfab35cad5fe56f4a0cf0e91571de76
Zireael07 <zi@mail.com>
2021-01-11 16:48:37 +0100

    new: beginnings of AI (jerky movements)

--- AI.gd
+++ AI.gd
@@ -7,22 +7,44 @@ var possible_tg = null
 var dead = false
 var carried = false
 
+var brain
+
+# see player.gd
+const GRAVITY = -24.8
+var vel = Vector3()
+const MAX_SPEED = 20
+const JUMP_SPEED = 18
+const ACCEL = 4 #4.5
+
+var dir = Vector3()
+
+const DEACCEL= 10 #16
+const MAX_SLOPE_ANGLE = 40
+
+var STEER_SENSITIVITY = 0.1 #0.05
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var mesh = get_node("RotationHelper/Character2/Armature/Body")
+	
+	brain = $brain
+	
+	# get points
+	brain.target = get_parent().get_node("nav").get_child(0)
+	
 	# visualize aabb
-	var debug = $RotationHelper/Character2/Armature/HitBoxTorso/center
-	for i in range(7):
-		var end_point = mesh.get_aabb().get_endpoint(i) # local space
-		 # because we're looking at relative to center
-		var point = to_global(end_point)
-		#var pt = Position3D.new()
-		var msh = CubeMesh.new()
-		msh.size = Vector3(0.25, 0.25, 0.25)
-		var pt = MeshInstance.new()
-		pt.set_mesh(msh)
-		debug.add_child(pt)
-		pt.global_transform.origin =  point
+#	var debug = $RotationHelper/Character2/Armature/HitBoxTorso/center
+#	for i in range(7):
+#		var end_point = mesh.get_aabb().get_endpoint(i) # local space
+#		 # because we're looking at relative to center
+#		var point = to_global(end_point)
+#		#var pt = Position3D.new()
+#		var msh = CubeMesh.new()
+#		msh.size = Vector3(0.25, 0.25, 0.25)
+#		var pt = MeshInstance.new()
+#		pt.set_mesh(msh)
+#		debug.add_child(pt)
+#		pt.global_transform.origin =  point
 
 
 
@@ -30,11 +52,77 @@ func _ready():
 #func _process(delta):
 #	pass
 
+# see player.gd
+func process_movement(delta):
+	# Assure our movement direction on the Y axis is zero, and then normalize it.
+	dir.y = 0
+	dir = dir.normalized()
+	# Apply gravity
+	vel.y += delta * GRAVITY
+	# Set our velocity to a new variable (hvel) and remove the Y velocity.
+	var hvel = vel
+	hvel.y = 0
+
+	var target = dir
+	target *= MAX_SPEED
+
+	# If we have movement input, then accelerate.
+	# Otherwise we are not moving and need to start slowing down.
+	var accel
+	if dir.dot(hvel) > 0:
+		accel = ACCEL
+	else:
+		accel = DEACCEL
+	#print("Accel, ", accel)
+	# Interpolate our velocity (without gravity), and then move using move_and_slide
+	hvel = hvel.linear_interpolate(target, accel * delta)
+	vel.x = hvel.x
+	vel.z = hvel.z
+	# infinite inertia is now false for better physics when colliding with objects
+	vel = move_and_slide(vel, Vector3(0, 1, 0), 0.05, 4, deg2rad(MAX_SLOPE_ANGLE), false)
+	
+	# https://kidscancode.org/godot_recipes/physics/kinematic_to_rigidbody/
+	# after calling move_and_slide()
+	for index in get_slide_count():
+		var collision = get_slide_collision(index)
+		if collision.collider.is_in_group("interactable"):
+			collision.collider.apply_central_impulse(-collision.normal * 2)
+
 func _physics_process(delta):	
 	if dead and not carried:
 		# attempt to sync collision with ragdoll
 		global_transform.origin = get_node("RotationHelper/Character2/Armature/HitBoxPelvis/Area/CollisionShape").global_transform.origin
 		return
+		
+	# movement
+	brain.steer = brain.arrive(brain.target, 5)
+	
+	# Reset dir, so our previous movement does not effect us
+	dir = Vector3()
+	
+	# Create a vector for storing input
+	var input_movement_vector = Vector2()
+	# steer y means forward/backwards
+	if brain.steer.y > 0:
+		input_movement_vector.y = 1
+	if brain.steer.y < 0:
+		input_movement_vector.y = -1
+	#print("input: ", input_movement_vector)
+	
+	# Normalize the input movement vector so we cannot move too fast
+	input_movement_vector = input_movement_vector.normalized()
+	
+	#print("AI input vec:", input_movement_vector)
+	
+	# Basis vectors are already normalized.
+	dir += get_global_transform().basis.z * input_movement_vector.y
+	
+	# rotations if any
+	self.rotate_y(deg2rad(brain.steer.x * STEER_SENSITIVITY))  #* -1))
+	
+	process_movement(delta)
+		
+	
 		
 	if not possible_tg:
 		return
---
+++ boid.gd
@@ -0,0 +1,93 @@
+# steering behaviors implementation
+extends Spatial
+
+# class member variables go here, for example:
+var velocity = Vector2(0,0)
+var steer = Vector2(0,0)
+var desired = Vector2(0,0)
+
+var MAX_FORCE = 4
+
+var dist = 0.0
+
+# match the ones in player.gd
+const GRAVITY = -24.8
+const MAX_SPEED = 20
+const JUMP_SPEED = 18
+const ACCEL = 1 #4.5
+
+export(Vector2) var target = Vector2(800,700) # dummy
+
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	
+	pass
+
+func _physics_process(delta):
+	pass
+
+# ------------------------------------------
+# steering behaviors
+func seek(target):
+	# make the code universal
+	# can be passed both a vector2 or a node
+	if not typeof(target) == TYPE_VECTOR3:
+		if "translation" in target:
+			# steering behaviors operate in local space
+			var tr = to_local(target.get_global_transform().origin)
+			target = Vector2(tr.x, tr.z) #get_global_position())
+	
+#	print("tg: " + str(target))
+#	print("position: " + str(get_global_position()))
+	
+	var steering = Vector2(0,0)
+	#print("Tg: " + str(target_obj.get_position()) + " " + str(get_position()))
+	
+	desired = target - Vector2(get_translation().x, get_translation().z)
+	dist = desired.length()
+#	print("des: " + str(desired))
+	desired = desired.normalized() * MAX_SPEED
+	#print("max speed des: " + str(desired))
+	#print("vel " + str(velocity))
+	# desired minus current vel
+	steering = (desired - velocity).clamped(MAX_FORCE)
+	#print(str(steering))
+	#steering = steering.clamped(max_force)
+	#print(str(steering))
+	
+	return(steering)
+
+func arrive(target, slowing_radius):
+	# make the code universal
+	# can be passed both a vector2 or a node
+	if not typeof(target) == TYPE_VECTOR3:
+		if "translation" in target:
+			# steering behaviors operate in local space
+			var tr = to_local(target.get_global_transform().origin)
+			target = Vector2(tr.x, tr.z) #get_global_position())
+	
+	var steering = Vector2(0,0)
+	#print("Arrive @: " + str(target) + " " + str(get_translation()))
+
+	desired = target - Vector2(get_translation().x, get_translation().z)
+	#print("Desired " + str(desired))
+	dist = desired.length()
+	#print("Dist: " + str(dist))
+	
+	if dist < slowing_radius:
+#		print("Slowing... " + str(dist/slowing_radius))
+		# inside slowing area
+		desired = desired.normalized() * MAX_SPEED * (dist / slowing_radius)
+		
+	else:
+		#print("Not slowing")
+		# outside
+		desired = desired.normalized() * MAX_SPEED
+
+	# desired minus current vel
+	steering = (desired - velocity).clamped(MAX_FORCE)
+	print("Steering", steering)
+
+	return (steering)
