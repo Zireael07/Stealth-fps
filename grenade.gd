9fe8e364535f1b3a5ce9b19c2d27b7f4becdf2c8
Zireael07 <zi@mail.com>
2021-11-22 15:10:12 +0100

    new: a simple implementation of a flashbang (blinds player, AI drops gun)

--- grenade.gd
+++ grenade.gd
@@ -4,27 +4,41 @@ extends RigidBody
 # Declare member variables here. Examples:
 var armed
 var possible_tg
-
+var player = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player = get_tree().get_nodes_in_group("player")[0]
 
 func explode():
+	print("Explode!")
 	# deal damage
 	if possible_tg:
 		possible_tg.die()
 
 	queue_free()
 
+func flash():
+	print("Timer over!!!")
+
+	# flash the player
+	player.get_node("Control/flashbang").show()
+	player.get_node("Control/AnimationPlayer").play("flash")
+
+	if possible_tg:
+		possible_tg.drop_gun()
+
+	queue_free()
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 
-func _on_Timer_timeout():
-	print("Timer over!!!!")
-	explode()
+#func _on_Timer_timeout():
+	#print("Timer over!!!!")
+#	explode()
 	#pass # Replace with function body.
 
 
