0ee8c4e7fa9bf8fb54e2354c75700360e7de2b1d
Zireael07 <zi@mail.com>
2022-03-19 14:35:33 +0100

    new: add a simple explosion effect to the grenade

--- weapons/grenade.gd
+++ weapons/grenade.gd
@@ -12,18 +12,38 @@ var attach_point = null
 var slot # for inventory
 var droppable = true
 
+# Using variables instead of a timer due to very low timers
+# The amount of time needed (in seconds) to wait so we can destroy the grenade after the explosion
+# (Calculated by taking the particle life time and dividing it by the particle's speed scale)
+const EXPLOSION_WAIT_TIME = 0.48
+# A variable for tracking how much time has passed since this grenade exploded
+var explosion_wait_timer = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_tree().get_nodes_in_group("player")[0]
 
+func explode_effect():
+	get_node("Explosion").emitting = true
+	# start the countup of explosion_effect
+	explosion_wait_timer = 0.05
+	
+	# Make the grenade mesh invisible, and disable the collision shape for the RigidBody
+	$Grenade2.visible = false
+	$CollisionShape.disabled = true
+			
+	# Set the RigidBody mode to static so it does not move
+	mode = RigidBody.MODE_STATIC
+	
 func explode():
 	print("Explode!")
+	explode_effect()
 	# deal damage
 	for b in possible_tg:
 		if b.is_in_group("AI"):
 			b.die(get_global_transform().origin)
 
-	queue_free()
+	#queue_free()
 
 func flash():
 	print("Timer over!!!")
@@ -56,7 +76,14 @@ func _process(delta):
 	if attached == true:
 		if attach_point != null:
 			global_transform.origin = attach_point.global_transform.origin
-#	pass
+	
+	# See if we need to free the grenade
+	if explosion_wait_timer > 0.04 and explosion_wait_timer < EXPLOSION_WAIT_TIME:
+		# count up
+		explosion_wait_timer += delta
+		
+		if explosion_wait_timer >= EXPLOSION_WAIT_TIME:
+			queue_free()
 
 
 #func _on_Timer_timeout():
